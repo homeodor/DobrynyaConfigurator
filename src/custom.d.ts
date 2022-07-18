2b8a0d7642768d9cc512621b7eb5ee913d3a453a
Alexander Golovanov <45916903+homeodor@users.noreply.github.com>
2022-07-18 12:03:27 +0000

    Added typedefs for custom body events

---
+++ src/custom.d.ts
@@ -0,0 +1,8 @@
+declare namespace svelte.JSX {
+    interface HTMLProps<T> {
+        ondobrynyahere?: (ev: CustomEvent) => any,
+        ondobrynyagone?: (ev: CustomEvent) => any,
+        oninvoke?: (ev: CustomEvent) => any,
+        onsection?: (ev: CustomEvent) => any,
+    }
+}
