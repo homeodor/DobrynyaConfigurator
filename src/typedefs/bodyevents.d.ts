a36fd6177d3062cd73861f1823b45c8a5b49d679
Alexander Kopf <i.am@shurik.uk>
2023-05-01 09:07:20 +0000

    Typedefs

---
+++ src/typedefs/bodyevents.d.ts
@@ -0,0 +1,22 @@
+declare namespace svelte.JSX {
+	interface HTMLProps<T> {
+		ondobrynyahere?: (ev: CustomEvent) => any,
+		ondobrynyagone?: (ev: CustomEvent) => any,
+		oninvoke?: (ev: CustomEvent) => any,
+		onsection?: (ev: CustomEvent) => any,
+		onpatchlock?: (ev: CustomEvent) => any,
+		onpatchchange?: (ev: CustomEvent) => any,
+		onsysexpush?: (ev: CustomEvent) => any,
+		ondrawer?: (ev: CustomEvent) => any,
+		oninvokebank?: (ev: CustomEvent) => any,
+		onselectpatch?: (ev: CustomEvent) => any,
+		onopennewui?: (ev: CustomEvent) => any,
+		oncloseeditor?: (ev: CustomEvent) => any,
+		onclosenewui?: (ev: CustomEvent) => any,
+	};
+}
+
+
+
+
+
