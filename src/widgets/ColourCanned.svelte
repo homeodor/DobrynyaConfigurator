9161e7715b4d9c248f048a8416bac65797c1daa1
Alexander Golovanov <a.golovanov@atoldrive.ru>
2025-09-24 14:10:01 +0300

    Colour WIP

--- src/widgets/ColourCanned.svelte
+++ src/widgets/ColourCanned.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
-	import { colourOff, hexToCSS, isSameColour } from '../ts/colour_utils';
-	
-	const dispatch = createEventDispatcher();
+	import { hexToCSS, isSameColour } from "../ts/colour_utils";
 
-	export let className: string = "";
-	export let selectedHex: number = colourOff;
-	export let hex: number = colourOff;
-	
-	function thisHex(ev: MouseEvent)
-	{
-		if (ev.type == "click" || ev.buttons & 0x1) selectedHex = hex;
+	let {
+		className = "",
+		selectedHex = $bindable(),
+		hex,
+	}: {
+		className?: string;
+		selectedHex: number;
+		hex: number;
+	} = $props();
+
+	function thisHex(ev: MouseEvent) {
+		if (ev.type == "click" || ev.buttons & 0x1) {
+			selectedHex = hex;
+		}
 	}
-	
 </script>
 
-	<!-- svelte-ignore a11y-mouse-events-have-key-events --><!-- svelte-ignore a11y-click-events-have-key-events -->
-	<div class={className} class:thecolour={isSameColour(hex,selectedHex)} style="background-color: {hexToCSS(hex)}" on:click={thisHex} on:mouseover={thisHex}></div>
+<!-- svelte-ignore a11y-mouse-events-have-key-events --><!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+	class={className}
+	class:thecolour={isSameColour(hex, selectedHex)}
+	style="background-color: {hexToCSS(hex)}"
+	onclick={thisHex}
+	onmouseover={thisHex}
+></div>
